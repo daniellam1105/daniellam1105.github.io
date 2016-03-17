c690b506fd56cfd6a688be294ed80cb0f4912faf
Daniella <daniellamorinigo@Daniellas-Comp.office1.viinetwork.net>
2016-03-17 12:47:30 -0400

    Adding my existing site.

--- js/app.js
+++ js/app.js
@@ -25,20 +25,7 @@ var app = (function(document, $) {
 
 })();
 
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
+
 
 function showDiv(idInfo) {
   var sel = document.getElementById('divLinks').getElementsByTagName('div');
